bfedee0a55b2469feaee8636e76cf0fe10f094a4
Sambeet Das <sambeet.gugul@gmail.com>
2025-07-16 20:11:39 +0530

    api code added

--- rag_api.py
+++ rag_api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from rag_pipeline import RAGPipeline
+import os   
 
 
 class RequestModel(BaseModel):
@@ -9,5 +11,16 @@ app = FastAPI()
 
 @app.post("/")
 async def create_item(model: RequestModel):
-    return model
+    MONGODB_URI = os.getenv("MONGODB_URI")
+    DATABASE_NAME = os.getenv("DATABASE_NAME") 
+    COLLECTION_NAME = os.getenv("COLLECTION_NAME")  
+    EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+    rag = RAGPipeline(
+        mongodb_uri=MONGODB_URI,
+        database_name=DATABASE_NAME,
+        collection_name=COLLECTION_NAME,
+        embedding_model = EMBEDDING_MODEL
+    )
+    response = await rag.get_answer(model.question, limit=3)
+    return response
 
